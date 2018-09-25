6eda6ea06e10fd9b926d0215cc483b293cad0147
Zabrina L <zabrinatan@hotmail.com>
2018-09-25 21:03:00 +1000

    added tests for links in homepage to about and contacts

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,6 +1,7 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit, currentURL, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+
 
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
@@ -11,9 +12,15 @@ module('Acceptance | list rentals', function(hooks) {
    });
 
    test('should link to information about the company.', async function (assert) {
+     await visit('/');
+     await click(".menu-about");
+     assert.equal(currentURL(), '/about', "should link to about")
    });
 
    test('should link to contact information.', async function (assert) {
+     await visit('/');
+     await click('.menu-contact');
+     assert.equal(currentURL(), '/contact', 'should link to contact page')
    });
 
    test('should list available rentals.', async function (assert) {
